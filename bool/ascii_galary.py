ef71cc84004b4f740bcbbe1b5d02f17f92dfe929
timealtair <83885014+timealtair@users.noreply.github.com>
2025-01-07 20:53:39 +0400

    ascii_galary temlate

---
+++ bool/ascii_galary.py
@@ -0,0 +1,27 @@
+star = r"""
+      __/\__ 
+      \    /   
+__/\__/    \__/\__
+\                /
+/_              _\
+  \            /
+__/            \__ 
+\                /
+/_  __      __  _\
+  \/  \    /  \/
+      /_  _\
+        \/"""
+cube = r"""
+   .+------+
+ .' |      |
++   |      |
+|   |      |
+|  .+------+
+|.'      .' 
++------+'   
+"""
+# print(star)
+# print(cube)
+print("What do you what to see? (Enter number)")
+print("1. star")
+print("2. cube")
