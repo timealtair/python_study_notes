e0da09081eb5bc6c6be521bc238d177bb29fe5f7
timealtair <83885014+timealtair@users.noreply.github.com>
2025-02-05 13:02:55 +0400

    prettified output

--- 5.expr_vs_stat/count_to.py
+++ 5.expr_vs_stat/count_to.py
@@ -1,8 +1,16 @@
+separator = '-' * 32
+
+print(separator)
+
 print('Please enter nubmer to count to:')
 count_to = input()
 count_to = int(count_to)
+
+print(separator)
 
 counter = 0
 while counter < count_to:
     counter = counter + 1
     print(counter)
+
+print(separator)
