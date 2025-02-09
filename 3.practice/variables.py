1a250bcfbd2e27b7e9d1f776aac082c7222311ec
timealtair <83885014+timealtair@users.noreply.github.com>
2025-02-09 18:21:38 +0400

    added 'no_variables.py'

---
+++ 3.practice/variables.py
@@ -0,0 +1,5 @@
+name = input('Hello, please write your name: ') 
+greeting = 'Hello ' + name + '! '
+times = int(input('Write how many times I should greet you: '))
+multi_grettings = greeting * times
+print(multi_grettings)
