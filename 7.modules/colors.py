8395b68167af0be680bffb8741bc1f76e2c711d2
timealtair <83885014+timealtair@users.noreply.github.com>
2025-02-13 13:07:12 +0400

    added cyan func

--- 7.modules/colors.py
+++ 7.modules/colors.py
@@ -12,8 +12,21 @@ def blue(object):
 
     return colour + str(object) + ENDCOLOR
 
+def cyan(object):
+    """
+    Function colors text in cyan.
+    """
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    ENDCOLOR = '\033[0m'
+
+    colour = CYAN
+
+    return colour + str(object) + ENDCOLOR
+
 
 
 
 
 print(blue('blue'))
+print(cyan('blue'))
