9b1bc3d769d907ab29f027d049671d8c7e61d5bd
timealtair <83885014+timealtair@users.noreply.github.com>
2025-02-13 13:31:10 +0400

    added funcs: pink green yellow red

--- 7.modules/colors.py
+++ 7.modules/colors.py
@@ -1,6 +1,11 @@
+_ENDCOLOR = '\033[0m'
 _BLUE = '\033[94m'
 _CYAN = '\033[96m'
-_ENDCOLOR = '\033[0m'
+_PINK = '\033[95m'
+_GREEN = '\033[92m'
+_YELLOW = '\033[93m'
+_RED = '\033[91m'
+
 
 def _color_text(object, color_const):
     return color_const + str(object) + _ENDCOLOR
@@ -17,7 +22,33 @@ def cyan(object):
     """
     return _color_text(object, _CYAN)
 
+def pink(object):
+    """
+    Function colors text in pink.
+    """
+    return _color_text(object, _PINK)
 
+def green(object):
+    """
+    Function colors text in green.
+    """
+    return _color_text(object, _GREEN)
+
+def yellow(object):
+    """
+    Function colors text in yellow.
+    """
+    return _color_text(object, _YELLOW)
+
+def red(object):
+    """
+    Function colors text in red.
+    """
+    return _color_text(object, _RED)
 
 print(blue('blue'))
-print(cyan('blue'))
+print(cyan('cyan'))
+print(pink('pink'))
+print(green('green'))
+print(yellow('yellow'))
+print(red('red'))
