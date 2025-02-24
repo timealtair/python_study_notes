e61089ffb9b83f612d5f62db75630a477e945a48
83885014+timealtair@users.noreply.github.com <timealtair>
2025-02-24 16:27:17 +0400

    added self test if __name__

--- 8.classes/wallet.py
+++ 8.classes/wallet.py
@@ -3,7 +3,7 @@ class Wallet:
     def __init__(self, start_amount):
         self._amount = start_amount
 
-    def get_amount(self):
+    def chech_amount(self):
         return self._amount
 
     def add_money(self, add_amount):
@@ -12,3 +12,16 @@ class Wallet:
     def take_money(self, take_amount):
         self._amount = self._amount - take_amount
         return take_amount
+
+
+if __name__ == '__main__':
+    my_wallet = Wallet(0)
+    print('Initial balance:')
+    print(my_wallet.chech_amount())
+    my_wallet.add_money(100)
+    print('Current balance after adding 100:')
+    print(my_wallet.chech_amount())
+    print('Taked money:')
+    print(my_wallet.take_money(40))
+    print('Current balance:')
+    print(my_wallet.chech_amount())
